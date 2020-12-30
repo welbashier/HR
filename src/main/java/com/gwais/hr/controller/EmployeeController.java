82320293f7466acbef8010f35415b1db4698b68d
Waleed E <welbashier@hotmail.com>
2020-12-30 00:32:53 -0600

    added an error page which is not connected yet

--- src/main/java/com/gwais/hr/controller/EmployeeController.java
+++ src/main/java/com/gwais/hr/controller/EmployeeController.java
@@ -21,29 +21,34 @@ public class EmployeeController {
 	@Autowired
 	IEmployeeService employeeService;
 
+	// accessed by: http://localhost:8011/Employee/Status
 	@GetMapping("/Status")
 	public String status() {
 		return "Up";
 	}
 
+	// accessed by: http://localhost:8011/Employee
 	@GetMapping("")
 	public List<EmployeeDto> employeeAPINo10() {
 		List<EmployeeDto> employees = employeeService.getAllEmployees();
 		return employees;
 	}
 
+	// accessed by: http://localhost:8011/Employee/7566
 	@GetMapping("/{id}")
 	public EmployeeDto employeeAPINo1(@PathVariable Long id) {
 		EmployeeDto readEmployee = employeeService.getOneEmployee(id);
 		return readEmployee;
 	}
 
+	// accessed by API client (e.g. Postman) to post/add employees
 	@PostMapping("/")
 	public EmployeeDto employeeAPINo12(@RequestBody EmployeeDto changedEmployee) {
 		EmployeeDto addedEmployee = employeeService.addEmployee(changedEmployee);
 		return addedEmployee;
 	}
 
+	// accessed by API client (e.g. Postman) to put/update employees
 	@PutMapping("/{id}") // may not be needed
 	public EmployeeDto employeeAPINo3(
 			@RequestBody EmployeeDto changedEmployee,
@@ -51,6 +56,7 @@ public class EmployeeController {
 		return employeeService.modifyEmployee(changedEmployee, id);
 	}
 
+	// accessed by API client (e.g. Postman) to delete employees
 	@DeleteMapping("/{id}")
 	public String employeeAPINo4(@PathVariable Long id) {
 		return employeeService.removeEmployee(id);
--- src/test/java/com/gwais/hr/dao/IDepartmentDaoTest.java
+++ src/test/java/com/gwais/hr/dao/IDepartmentDaoTest.java
@@ -9,7 +9,6 @@ import javax.persistence.EntityNotFoundException;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
