604fcf7b7db9d88bb7c2a2c0d4aeb6c59b358def
Waleed E <welbashier@hotmail.com>
2021-03-27 02:14:04 -0500

    successfully added the custom error pages

--- src/main/java/com/gwais/hr/config/HrSecurityConfigurerAdapter.java
+++ src/main/java/com/gwais/hr/config/HrSecurityConfigurerAdapter.java
@@ -7,17 +7,19 @@ import org.springframework.security.config.annotation.web.configuration.*;
 @Configuration
 @EnableWebSecurity
 public class HrSecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+				.antMatchers("/Employee/home")	.access("hasRole('ADMIN') or hasRole('USER')")
+				.antMatchers("/Employee/admin")	.access("hasRole('ADMIN')")
+				.antMatchers("/Employee/{id}")	.access("hasRole('USER')")
+				.anyRequest().authenticated()
+				.and()
+				.formLogin().permitAll()
+				.and()
+				.logout().permitAll()
+				;
+		}
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-            .antMatchers("/Employee/home").access("hasRole('USER') or hasRole('ADMIN')")
-            .antMatchers("/Employee/admin").access("hasRole('ADMIN')")
-            .anyRequest().authenticated()
-            .and().formLogin().permitAll()
-            .and().logout().permitAll()
-            .and().exceptionHandling().accessDeniedPage("/403")
-            ;
-    }
-    
 }
