07525d1c3f6a541871ad7333d0ae4d9991abae36
Waleed E <welbashier@hotmail.com>
2021-03-28 19:48:25 -0500

    adding more default users

--- src/main/java/com/gwais/hr/config/HrSecurityConfigurerAdapter.java
+++ src/main/java/com/gwais/hr/config/HrSecurityConfigurerAdapter.java
@@ -1,8 +1,11 @@
 package com.gwais.hr.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.*;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.*;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.thymeleaf.extras.springsecurity5.dialect.SpringSecurityDialect;
 import org.thymeleaf.spring5.SpringTemplateEngine;
 import org.thymeleaf.templateresolver.ITemplateResolver;
@@ -33,5 +36,19 @@ public class HrSecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
 		templateEngine.addDialect(sec); // Enable use of "sec" return templateEngine;
 		return (templateEngine);
 	}
-	 
+	
+	@Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) 
+      throws Exception {
+        auth
+          .inMemoryAuthentication()
+          .withUser("zoalsai").password(passwordEncoder().encode("tamaam")).roles("USER")
+          .and()
+          .withUser("modeer").password(passwordEncoder().encode("admin")).roles("ADMIN");
+    }
+    
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
