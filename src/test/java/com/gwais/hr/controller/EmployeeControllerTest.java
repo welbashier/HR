6441d2b3a369a6349276c4725ba465d589112a8c
Waleed E <welbashier@hotmail.com>
2021-12-01 22:02:21 -0600

    added a junit test to test the Employee controller

---
+++ src/test/java/com/gwais/hr/controller/EmployeeControllerTest.java
@@ -0,0 +1,44 @@
+package com.gwais.hr.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class EmployeeControllerTest {
+
+	@Test
+	void testStatus() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testEmployeeAPINo10() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testEmployeeAPINo1() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testEmployeeAPINo3() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testEmployeeAPINo4() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testWelcomePage() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testAdminPage() {
+		fail("Not yet implemented");
+	}
+
+}
